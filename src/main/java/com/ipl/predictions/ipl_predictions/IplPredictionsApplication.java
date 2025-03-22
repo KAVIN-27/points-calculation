2a4e4541b3150cfbe4f6afc673b5a647f06d81ed
Kavin <kavin1615yoga@gmail.com>
2025-03-22 12:25:36 +0530

    inital commit

---
+++ src/main/java/com/ipl/predictions/ipl_predictions/IplPredictionsApplication.java
@@ -0,0 +1,13 @@
+package com.ipl.predictions.ipl_predictions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplPredictionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplPredictionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipl/predictions/ipl_predictions/IplPredictionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipl.predictions.ipl_predictions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplPredictionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
